1d6ed656da4815a4f0bdb62ffa1e10a11e0c4105
Anna Lyons <Anna.Lyons@nicta.com.au>
2015-08-11 11:23:53 +1000

    refactor arch specific code into arch directories, leveraging the build system rather than #ifdef

---
+++ apps/sel4bench/include/arch/arm/armv/armv7-a/armv/ipc.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(NICTA_GPL)
+ */
+#ifndef __ARCH_ARMV_H
+#define __ARCH_ARMV_H
+
+#define READ_COUNTER(var) do { \
+    asm volatile("mrc p15, 0, %0, c9, c13, 0\n" \
+        : "=r"(var) \
+    ); \
+} while(0)
+
+#endif /* __ARCH_ARMV_H */
+
