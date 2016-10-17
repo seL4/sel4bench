a12cad3d24416345ced227abbfd1376eda7a4206
Anna Lyons <Anna.Lyons@data61.csiro.au>
2016-10-17 16:44:01 +1100

    Add plat dir to sel4bench, set sabre freq
    - add plat dir for plat specific init
    - make sabre plat specific init set the freq to highest
    - move pc99 specific code from main to plat dir
    - stubs for other platforms

---
+++ apps/sel4bench/src/plat/imx6/plat.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+#include <platsupport/clock.h>
+#include <utils/frequency.h>
+
+#include "../../env.h"
+
+#define IMX6_MAX_FREQ (996 * MHZ)
+
+void
+plat_setup(env_t *env)
+{
+    /* set to highest cpu freq */
+    int error = sel4platsupport_new_io_mapper(env->vspace, env->vka, &env->ops.io_mapper);
+    ZF_LOGF_IF(error != 0, "Failed to get io mapper");
+
+    clock_sys_t clock;
+    error = clock_sys_init(&env->ops, &clock);
+	ZF_LOGF_IF(error != 0, "clock_sys_init failed");
+
+    clk_t *clk = clk_get_clock(&clock, CLK_ARM);
+    ZF_LOGF_IF(error != 0, "Failed to get clock");
+
+    freq_t freq = clk_set_freq(clk, IMX6_MAX_FREQ);
+	ZF_LOGF_IF(freq != IMX6_MAX_FREQ, "Failed to set imx6 freq");
+}
---
+++ apps/sel4bench/src/plat/pc99/plat.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2016, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+#include "../../env.h"
+
+void
+plat_setup(env_t *env)
+{
+ 	if (config_set(CONFIG_ARCH_X86_64)) {
+     	/* check if the cpu supports rdtscp */
+    	int edx = 0;
+	    asm volatile("cpuid":"=d"(edx):"a"(0x80000001):"ecx");
+      	ZF_LOGF_IF((edx & (BIT(27))) == 0, "CPU does not support rdtscp instruction");
+ 	}
+}
