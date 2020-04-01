91f05c1b7bc3867d70c25f182092abacdc22b87e
Nils Wistoff <git@wistoff.net>
2020-04-01 18:27:59 +0200

    riscv: add hardware benchmark support
    Add RISC-V support for the hardware benchmark application
    
    Signed-off-by: Nils Wistoff <git@wistoff.net>

--- libsel4benchsupport/arch_include/riscv/arch/hardware.h
+++ libsel4benchsupport/arch_include/riscv/arch/hardware.h
@@ -11,4 +11,12 @@
  */
 #pragma once
 
+#define DO_NULLSYSCALL_OP(swi, syscall) do {\
+    register seL4_Word scno asm("a7") = syscall; \
+    asm volatile (NOPS swi NOPS :: "r"(scno)); \
+} while (0);
+
 #define DO_NULLSYSCALL(swi) DO_NULLSYSCALL_OP(swi, seL4_SysBenchmarkNullSyscall)
+
+#define DO_REAL_NULLSYSCALL()     DO_NULLSYSCALL("ecall")
+#define DO_NOP_NULLSYSCALL()      DO_NULLSYSCALL("nop")
