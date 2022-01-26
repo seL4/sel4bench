0d21b6bf0e309fb4810948d3ad41dc6bca949f12
Nataliya Korovkina <malus.brandywine@gmail.com>
2022-01-26 16:11:54 -0500

    add project options to switch benchmarks on-off
    easy-settings.cmake was amended by configuration options to choose
    benchmark applications to be included in the image.
    The settings should be the same as local defaults in corresponding
    app directories.
    
    easy-settings.cmake introduces simplified names of the options.
    Matching of the simplified names to the option names in local
    directories can be found in settings.cmake
    (Ex. "SCHED" - "AppSchedulerBench")
    
    Signed-off-by: Nataliya Korovkina <malus.brandywine@gmail.com>

--- easy-settings.cmake
+++ easy-settings.cmake
@@ -20,11 +20,21 @@
 # ninja
 #
 set(RELEASE ON CACHE BOOL "Performance optimized build")
-set(HARDWARE OFF CACHE BOOL "Configuration for sel4bench hardware app")
-set(FAULT ON CACHE BOOL "Configuration sel4bench fault app")
-set(VCPU OFF CACHE BOOL "Whether or not to run the VCPU benchmarks")
-set(SMP OFF CACHE BOOL "Configuration sel4bench smp app")
 set(PLATFORM "x86_64" CACHE STRING "Platform to test")
 set(FASTPATH ON CACHE BOOL "Turn fastpath on or off")
 set(ARM_HYP OFF CACHE BOOL "ARM EL2 hypervisor features on or off")
 set(MCS OFF CACHE BOOL "MCS kernel")
+
+# Set the list of benchmark applications to be included into the image
+set(HARDWARE OFF CACHE BOOL "Application to benchmark hardware-related operations")
+set(FAULT OFF CACHE BOOL "Application to benchmark seL4 faults")
+set(VCPU OFF CACHE BOOL "Application to benchmark seL4 VCPU performance")
+set(SMP OFF CACHE BOOL "Application SMP benchmarks")
+set(IPC ON CACHE BOOL "Application to benchmark seL4 IPC")
+# see apps/irq/CMakeLists.txt for current state of the benchmark
+set(IRQ OFF CACHE BOOL "Application to benchmark seL4 IRQs from inside the kernel")
+set(IRQUSER ON CACHE BOOL "Application to benchmark seL4 IRQs")
+set(SCHED ON CACHE BOOL "Application to benchmark seL4 scheduler")
+set(SIGNAL ON CACHE BOOL "Application to benchmark seL4 signals")
+set(MAPPING ON CACHE BOOL "Application to benchmark seL4 mapping a series of pages")
+set(SYNC ON CACHE BOOL "Application to benchmark seL4 sync")
