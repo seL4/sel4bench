61b4f8be22d2e29458f67c0bf86953a243b9460a
Nataliya Korovkina <malus.brandywine@gmail.com>
2022-05-09 17:26:06 -0400

    bench/signal: improve early processing loop
    Measurement loop for early processing was improved. The bitmask array
    was replaced with a single variable.
    
    Signed-off-by: Nataliya Korovkina <malus.brandywine@gmail.com>

--- apps/sel4bench/src/math.h
+++ apps/sel4bench/src/math.h
@@ -10,8 +10,23 @@
 
 result_t calculate_results(const size_t n, ccnt_t data[n]);
 
-result_t calculate_results_early_proc(ccnt_t num, ccnt_t min, ccnt_t max,
-                                      ccnt_t sum, ccnt_t sum2, ccnt_t array[num]);
+/*
+ * The function calculates parameters of array elements received from
+ * a benchmark which used early processing methodology
+ * @param num - number of samples
+ * @param sum - sum of samples
+ * @param sum2 - sum of squared samples
+ * @param array - array of raw data which are zeros for Early Processing methodology
+ * but the array is required for results output function
+ */
+result_t calculate_results_early_proc(ccnt_t num, ccnt_t sum, ccnt_t sum2,
+                                      ccnt_t array[num]);
 
-static double results_variance_early_proc(const size_t n, const ccnt_t sum,
+/* The function calculates variance using sum, sum of squared values and mean
+ * @param num - number of samples
+ * @param sum - sum of samples
+ * @param sum2 - sum of squared samples
+ * @param mean - mean of the samples
+*/
+static double results_variance_early_proc(const size_t num, const ccnt_t sum,
                                           const ccnt_t sum2, const ccnt_t mean);
--- libsel4benchsupport/include/sel4benchsupport/signal.h
+++ libsel4benchsupport/include/sel4benchsupport/signal.h
@@ -13,14 +13,12 @@
 
 typedef struct signal_results {
     ccnt_t lo_prio_results[N_RUNS];
-    ccnt_t lo_min;
-    ccnt_t lo_max;
-    ccnt_t lo_sum;
-    ccnt_t lo_sum2;
-    ccnt_t lo_num; /* number of samples to process */
-
     ccnt_t hi_prio_results[N_RUNS];
     ccnt_t overhead[N_RUNS];
-    ccnt_t overhead_min;
     ccnt_t hi_prio_average[N_RUNS][NUM_AVERAGE_EVENTS];
+    /* Parameters intrinsic to early processing */
+    ccnt_t lo_sum; /* sum of samples */
+    ccnt_t lo_sum2; /* sum of squared samples */
+    ccnt_t lo_num; /* number of samples to process */
+    ccnt_t overhead_min; /* min overhead found in "overhead" array */
 } signal_results_t;
