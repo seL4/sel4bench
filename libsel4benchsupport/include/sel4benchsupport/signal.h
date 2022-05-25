a314b3cc590f93f7003b5af6636d458f9a224c01
Nataliya Korovkina <malus.brandywine@gmail.com>
2022-05-25 15:57:05 -0400

    bench/signal: multiple code improvements
    Multiple code improvements
    
    Signed-off-by: Nataliya Korovkina <malus.brandywine@gmail.com>

--- libsel4benchsupport/include/sel4benchsupport/signal.h
+++ libsel4benchsupport/include/sel4benchsupport/signal.h
@@ -12,13 +12,17 @@
 #define N_RUNS (100 + N_IGNORED)
 
 typedef struct signal_results {
+    /* Data for late processing */
     ccnt_t lo_prio_results[N_RUNS];
     ccnt_t hi_prio_results[N_RUNS];
-    ccnt_t overhead[N_RUNS];
     ccnt_t hi_prio_average[N_RUNS][NUM_AVERAGE_EVENTS];
-    /* Parameters intrinsic to early processing */
+    ccnt_t overhead[N_RUNS]; /* "overhead" is used by early processing as well */
+    /* Data for early processing */
     ccnt_t lo_sum; /* sum of samples */
     ccnt_t lo_sum2; /* sum of squared samples */
     ccnt_t lo_num; /* number of samples to process */
     ccnt_t overhead_min; /* min overhead found in "overhead" array */
+    /* array required by report output function
+    Zeros, but can be used for diagnostic data */
+    ccnt_t diag_results[N_RUNS]; /* array required by report output function */
 } signal_results_t;
