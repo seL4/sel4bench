befeac565a6aa41f657de6736ae1315ad8b91da5
Gerwin Klein <gerwin.klein@proofcraft.systems>
2025-08-14 08:31:03 +0100

    trivial: update style to new cmake-format
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- easy-settings.cmake
+++ easy-settings.cmake
@@ -11,57 +11,101 @@
 #
 # Users should initialize a build directory by doing something like:
 #
-# mkdir build_sabre
-# cd build_sabre
+# mkdir build_sabre cd build_sabre
 #
 # Then
 #
-# ../griddle --PLATFORM=sabre --SIMULATION
-# ninja
+# ../griddle --PLATFORM=sabre --SIMULATION ninja
 #
-set(RELEASE ON CACHE BOOL "Performance optimized build")
-set(PLATFORM "x86_64" CACHE STRING "Platform to test")
-set(FASTPATH ON CACHE BOOL "Turn fastpath on or off")
-set(ARM_HYP OFF CACHE BOOL "ARM EL2 hypervisor features on or off")
-set(MCS OFF CACHE BOOL "MCS kernel")
+set(RELEASE
+    ON
+    CACHE BOOL "Performance optimized build"
+)
+set(PLATFORM
+    "x86_64"
+    CACHE STRING "Platform to test"
+)
+set(FASTPATH
+    ON
+    CACHE BOOL "Turn fastpath on or off"
+)
+set(ARM_HYP
+    OFF
+    CACHE BOOL "ARM EL2 hypervisor features on or off"
+)
+set(MCS
+    OFF
+    CACHE BOOL "MCS kernel"
+)
 
-# Set the list of benchmark applications to be included into the image
-# Add any new benchmark applications to this list
+# Set the list of benchmark applications to be included into the image Add any
+# new benchmark applications to this list
 
 # default is OFF
-set(HARDWARE OFF CACHE BOOL "Application to benchmark hardware-related operations")
+set(HARDWARE
+    OFF
+    CACHE BOOL "Application to benchmark hardware-related operations"
+)
 
 # default is OFF
-set(FAULT OFF CACHE BOOL "Application to benchmark seL4 faults")
+set(FAULT
+    OFF
+    CACHE BOOL "Application to benchmark seL4 faults"
+)
 
 # default is OFF
-set(VCPU OFF CACHE BOOL "Application to benchmark seL4 VCPU performance")
+set(VCPU
+    OFF
+    CACHE BOOL "Application to benchmark seL4 VCPU performance"
+)
 
 # default is OFF
-set(SMP OFF CACHE BOOL "Application SMP benchmarks")
+set(SMP
+    OFF
+    CACHE BOOL "Application SMP benchmarks"
+)
 
 # default is ON
-set(IPC ON CACHE BOOL "Application to benchmark seL4 IPC")
+set(IPC
+    ON
+    CACHE BOOL "Application to benchmark seL4 IPC"
+)
 
-# see apps/irq/CMakeLists.txt for current state of the benchmark
-# default is OFF
-set(IRQ OFF CACHE BOOL "Application to benchmark seL4 IRQs from inside the kernel")
-
-# default is ON
-set(IRQUSER ON CACHE BOOL "Application to benchmark seL4 IRQs")
-
-# default is ON
-set(SCHED ON CACHE BOOL "Application to benchmark seL4 scheduler")
-
-# default is ON
-set(SIGNAL ON CACHE BOOL "Application to benchmark seL4 signals")
+# see apps/irq/CMakeLists.txt for current state of the benchmark default is OFF
+set(IRQ
+    OFF
+    CACHE BOOL "Application to benchmark seL4 IRQs from inside the kernel"
+)
 
 # default is ON
-set(MAPPING ON CACHE BOOL "Application to benchmark seL4 mapping a series of pages")
+set(IRQUSER
+    ON
+    CACHE BOOL "Application to benchmark seL4 IRQs"
+)
 
 # default is ON
-set(SYNC ON CACHE BOOL "Application to benchmark seL4 sync")
+set(SCHED
+    ON
+    CACHE BOOL "Application to benchmark seL4 scheduler"
+)
 
-# Allow Early Processing methodology for
-#Signal/"Signal to High Prio Thread" benchmark
-#set(AppSignalEarlyProcessing ON)
+# default is ON
+set(SIGNAL
+    ON
+    CACHE BOOL "Application to benchmark seL4 signals"
+)
+
+# default is ON
+set(MAPPING
+    ON
+    CACHE BOOL "Application to benchmark seL4 mapping a series of pages"
+)
+
+# default is ON
+set(SYNC
+    ON
+    CACHE BOOL "Application to benchmark seL4 sync"
+)
+
+# Allow Early Processing methodology for Signal/"Signal to High Prio Thread"
+# benchmark set(AppSignalEarlyProcessing ON)
